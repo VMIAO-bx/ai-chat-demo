cfe629ead61e148185d060d2ecb157fb245532b0
VMIAO-bx <212497659@qq.com>
2026-04-25 11:16:22 +0800

    前后端合并到Railway

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const path = require('path');
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 
